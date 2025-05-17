b53e566eb09147b48da60be4c1bfbbfb316a54d9
birddograbbit <birddograbbit@gmail.com>
2025-05-17 02:28:32 +0200

    Add __init__.py for backtesting package

---
+++ src/backtesting/__init__.py
@@ -0,0 +1,9 @@
+"""
+Decision Tree Trading Strategy - Backtesting module
+
+This module contains components for backtesting trading strategies.
+"""
+
+from .engine import BacktestEngine
+
+__all__ = ["BacktestEngine"]

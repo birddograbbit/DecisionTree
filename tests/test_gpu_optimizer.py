7d83623d686359584f889f007888fc9dee1b6e8c
birddograbbit <birddograbbit@gmail.com>
2025-06-26 21:22:07 +0300

    Fix GPU optimizer tests and docs

--- tests/test_gpu_optimizer.py
+++ tests/test_gpu_optimizer.py
@@ -5,11 +5,10 @@ from src.models.transformer.gpu_optimizer import GPUOptimizedTransformer
 
 def test_mixed_precision_training():
     model = TimeSeriesTransformer(feature_size=3, seq_length=2)
-    crit = torch.nn.MSELoss()
     opt = torch.optim.Adam(model.parameters())
-    gpu_opt = GPUOptimizedTransformer(model, crit, device='cpu')
-    X = torch.randn(4,2,3)
-    y = torch.randn(4,1)
+    gpu_opt = GPUOptimizedTransformer(model, device='cpu')
+    X = torch.randn(4, 2, 3)
+    y = torch.randn(4, 1)
     ds = torch.utils.data.TensorDataset(X, y)
     loader = torch.utils.data.DataLoader(ds, batch_size=2)
     gpu_opt.fit(loader, opt, epochs=1)
